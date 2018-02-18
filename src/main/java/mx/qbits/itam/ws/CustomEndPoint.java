08a99b456442859d9aee39a9dcff0aa657fe70d7
Gustavo Adolfo Arellano Sandoval <garellano@MacBook-Pro-de-Gustavo-2.local>
2018-02-18 14:21:04 -0600

    First Version

---
+++ src/main/java/mx/qbits/itam/ws/CustomEndPoint.java
@@ -0,0 +1,95 @@
+package mx.qbits.itam.ws;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+import javax.websocket.server.ServerEndpoint;
+
+/* 
+https://examples.javacodegeeks.com/enterprise-java/tomcat/apache-tomcat-websocket-tutorial/
+https://dzone.com/articles/sample-java-web-socket-client
+http://www.eclipse.org/jetty/documentation/9.4.x/embedding-jetty.html
+https://examples.javacodegeeks.com/enterprise-java/jetty/jetty-websocket-example/
+*/
+
+@ServerEndpoint("/websocketendpoint")
+public class CustomEndPoint {
+    // queue holds the list of connected clients
+    private static Queue<Session> queue = new ConcurrentLinkedQueue<Session>();
+    private static Thread rateThread; // rate publisher thread
+    
+    static {
+        // rate publisher thread, generates a new value for USD rate every 2
+        // seconds.
+        rateThread = new Thread() {
+            public void run() {
+                DecimalFormat df = new DecimalFormat("#.####");
+                while (true) {
+                    double d = 2 + Math.random();
+                    if (queue != null)
+                        sendAll(df.format(d));
+                    try {
+                        sleep(2000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            };
+        };
+        rateThread.start();
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String msg) {
+        try {
+            System.out.println("received msg " + msg + " from " + session.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnOpen
+    public void open(Session session) {
+        queue.add(session);
+        System.out.println("New session opened: " + session.getId());
+    }
+
+    @OnError
+    public void error(Session session, Throwable t) {
+        queue.remove(session);
+        System.err.println("Error on session " + session.getId());
+    }
+
+    @OnClose
+    public void closedConnection(Session session) {
+        queue.remove(session);
+        System.out.println("session closed: " + session.getId());
+    }
+
+    private static void sendAll(String msg) {
+        try {
+            /* Send the new rate to all open WebSocket sessions */
+            ArrayList<Session> closedSessions = new ArrayList<Session>();
+            for (Session session : queue) {
+                if (!session.isOpen()) {
+                    System.err.println("Closed session: " + session.getId());
+                    closedSessions.add(session);
+                } else {
+                    session.getBasicRemote().sendText(msg);
+                }
+            }
+            queue.removeAll(closedSessions);
+            System.out.println("Sending " + msg + " to " + queue.size() + " clients");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/mx/qbits/itam/ws/Principal.java
@@ -0,0 +1,34 @@
+package mx.qbits.itam.ws;
+
+import javax.websocket.server.ServerContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+public class Principal {
+    public static void main(String...argv) {
+        try {
+            doit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void doit() throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/WebSocket");
+        server.setHandler(context);
+
+        // Solo se necesitan estas 2 lineas: 
+        ServerContainer wscontainer = WebSocketServerContainerInitializer.configureContext(context);
+        wscontainer.addEndpoint(CustomEndPoint.class);
+
+        server.start();
+        server.join();
+    }
+
+}
+
+
