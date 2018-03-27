ae23d51e187f7c1c397813213d87adbd5ad06f8c
arellano-gustavo <arellano.gustavo@gmail.com>
2018-03-26 22:48:15 -0600

    Update CustomEndPoint.java
    se corrige código que no compilaba

--- src/main/java/mx/qbits/itam/ws/CustomEndPoint.java
+++ src/main/java/mx/qbits/itam/ws/CustomEndPoint.java
@@ -33,7 +33,7 @@ public class CustomEndPoint {
             public void run() {
                 DecimalFormat df = new DecimalFormat("#.####");
                 while (true) {
-                    double d = gus + Math.random();
+                    double d = 1 + Math.random();
                     if (queue != null)
                         sendAll(df.format(d));
                     try {
