197da086546e5e962b762cacf11c94805e6b4392
garellano <garellano@localhost>
2018-04-16 20:42:30 -0500

    Se agrega una prueba mas para factorial

--- src/test/java/mx/qbits/itam/ws/TestCalcService.java
+++ src/test/java/mx/qbits/itam/ws/TestCalcService.java
@@ -11,6 +11,10 @@ public class TestCalcService {
         int val = 5;
         int result = cs.fact(val);
         assertTrue("Probando que el factorial de "+val+" es:", 120==result);
+        
+        val = 1;
+        result = cs.fact(val);
+        assertTrue("Probando que el factorial de "+val+" es:", 1==result);        
     }
 }
 
